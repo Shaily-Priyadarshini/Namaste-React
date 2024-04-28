d66c5c6891aa402a2e228943dbd9a6f27d3a9f5b
Shaily Priyadarshini <priyadarshinishaily@gmail.com>
2024-04-28 18:17:52 +0530

    Episode2+3

--- App.js
+++ App.js
@@ -1,22 +1,38 @@
-const parent=React.createElement(
-    "div",
-    {id:"parent"},
-  [  React.createElement(
-        "div",
-        {id:"child1"},
-        [React.createElement( "h1",{},"I am h1 tag"),React.createElement( "h2",{},"I am h2 tag")]),
-        React.createElement(
-            "div",
-            {id:"child2"},
-            [React.createElement( "h1",{},"I am h1 tag"),React.createElement( "h2",{},"I am h2 tag")])]
+import React from "react";
+import ReactDOM from "react-dom/client";
+// const parent=React.createElement(
+//     "div",
+//     {id:"parent"},
+//   [  React.createElement(
+//         "div",
+//         {id:"child1"},
+//         [React.createElement( "h1",{},"I am h1 tag"),React.createElement( "h2",{},"I am h2 tag")]),
+//         React.createElement(
+//             "div",
+//             {id:"child2"},
+//             [React.createElement( "h1",{},"I am h1 tag"),React.createElement( "h2",{},"I am h2 tag")])]
 
     
+// )
+
+// const heading=React.createElement("h1",{
+//     id:"heading",
+//     xyz:"abc"
+
+// },"hello world")
+
+const Title=()=>(
+    <h3>I am Shaily</h3>
 )
 
-const heading=React.createElement("h1",{
-    id:"heading",
-    xyz:"abc"
+const Heading=()=>{
+    return (
+        <div className="container">
+            <Title/>
+            <h1>Hi</h1>
+        </div>
+    )
 
-},"hello world")
+}
 const root=ReactDOM.createRoot(document.getElementById("root"));
-root.render(parent)
+root.render(<Heading />)
